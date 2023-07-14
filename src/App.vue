ef71b6ff3ec37c617e8207ee13b5e647078bca12
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-07-14 21:26:46 +0200

    update style

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import TheHeader from './components/TheHeader.vue'
 </script>
 
 <template>
-  <div class="min-h-screen bg-gray-100">
+  <div class="min-h-screen bg-gray-100 pb-10">
     <TheHeader />
     <main class="container mx-auto">
       <RouterView />
