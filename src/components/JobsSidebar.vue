d7eb77770e532c62b2bb109fd27595720c15d7bb
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-07-23 12:33:29 +0200

    create jobs sidebar

--- src/components/JobsSidebar.vue
+++ src/components/JobsSidebar.vue
@@ -1,3 +1,68 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+import AppButton from '@/components/AppButton.vue'
+import JobsSidebarDropdown from '@/components/JobsSidebarDropdown.vue'
+
+import { useJobsStore } from '../store/jobs'
+
+const router = useRouter()
+
+const store = useJobsStore()
+
+const selectedOrganizations = ref([])
+
+function handleSelectOrganization() {
+  store.addSelectedOrganizations(selectedOrganizations.value)
+  router.push({ name: 'jobs' })
+}
+
+const selectedJobTypes = ref([])
+
+function handleSelectJobType() {
+  store.addSelectedJobTypes(selectedJobTypes.value)
+  router.push({ name: 'jobs' })
+}
+</script>
+
 <template>
-  <div>Sidebar</div>
+  <aside class="w-max space-y-5">
+    <section class="flex items-center gap-5 overflow-hidden rounded bg-white p-4">
+      <span class="shrink-0 text-sm font-semibold text-gray-700">What do you want to do?</span>
+      <AppButton size="small">Clear filters</AppButton>
+    </section>
+
+    <JobsSidebarDropdown v-if="store.organizations.length" title="Organizations">
+      <div
+        v-for="(organization, index) in store.organizations"
+        :key="index"
+        class="flex items-center gap-2"
+      >
+        <input
+          :id="organization"
+          :value="organization"
+          v-model="selectedOrganizations"
+          @change="handleSelectOrganization"
+          class="relative -top-px accent-emerald-300"
+          type="checkbox"
+        />
+        <label :for="organization">{{ organization }}</label>
+      </div>
+    </JobsSidebarDropdown>
+
+    <JobsSidebarDropdown v-if="store.jobTypes.length" title="Job types">
+      <div v-for="(jobType, index) in store.jobTypes" :key="index" class="flex items-center gap-2">
+        <input
+          :id="jobType"
+          :value="jobType"
+          v-model="selectedJobTypes"
+          @change="handleSelectJobType"
+          class="relative -top-px accent-emerald-300"
+          type="checkbox"
+        />
+        <label :for="jobType">{{ jobType }}</label>
+      </div>
+    </JobsSidebarDropdown>
+  </aside>
 </template>
---
+++ src/components/JobsSidebarDropdown.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+import IconChevronDown from './icons/IconChevronDown.vue'
+import IconChevronUp from './icons/IconChevronUp.vue'
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+
+const isOpen = ref(false)
+
+function toggle() {
+  isOpen.value = !isOpen.value
+}
+</script>
+
+<template>
+  <section class="overflow-hidden rounded border text-sm">
+    <header
+      @click="toggle"
+      class="flex cursor-pointer items-center justify-between bg-gray-50 p-4 font-semibold text-gray-700"
+    >
+      <span>{{ title }}</span>
+      <component :is="isOpen ? IconChevronUp : IconChevronDown" />
+    </header>
+    <div v-show="isOpen" class="space-y-5 bg-white p-4 text-gray-600">
+      <slot />
+    </div>
+  </section>
+</template>
